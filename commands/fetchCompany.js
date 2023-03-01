795e9661ceb7e442f7e4bf4ff04b6f7de5090ad6
Fahad Hossain <84981998+Spit-fires@users.noreply.github.com>
2023-03-01 07:30:20 +0000

    add embed

--- commands/fetchCompany.js
+++ commands/fetchCompany.js
@@ -13,7 +13,38 @@ module.exports = {
   async execute(interaction) {
     const uid = interaction.options.getString('uid');
     const company = await companies.findOne({ where: { uid: uid } });
-    if (!company) return interaction.reply('Company not found, Error code 4000');
-    return interaction.reply(`Company with the following properties found: \nName: ${company.name} \nIndustry: ${company.industry} \nSalary: ${company.salary} \nCEO: ${company.ceo} \nBalance: ${company.bal} \nUID: ${company.uid}`);
+    if (!company) return interaction.reply('Company not found, Error code 4004');
+    const embed =  {
+      color: 0x0099ff,
+      title: 'Found information about ' + company.name,
+      fields: [
+        {
+          name: 'Name',
+          value: company.name,
+        },
+        {
+          name: 'Industry',
+          value: company.industry,
+        },
+        {
+          name: 'Salary',
+          value: company.salary,
+        },
+        {
+          name: 'CEO',
+          value: company.ceo,
+        },
+        {
+          name: 'Balance',
+          value: company.bal,
+        },
+        {
+          name: 'UID',
+          value: company.uid,
+        },
+      ],
+      timestamp: new Date(),
+    };
+    return interaction.reply({ embeds: [embed] });
   }
 };
